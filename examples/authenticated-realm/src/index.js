534e1db85b956056b7eb41be601b3ca472b4d314
Andy <andrew.mcmenemy03@gmail.com>
2020-04-16 14:33:26 +1000

    feat: CHARTS 3579 Realm Auth Sample (#1)
    * feat:CHARTS-3579 first draft
    
    * CHARTS-3579: vertical login page
    
    * CHARTS-3579: dual pane draft
    
    * CHJARTS-3579: final design
    
    * CHARTS-3579: Added a readme
    
    * fix:CHARTS-3579 finished off Realm user role setup steps
    
    * Added logout feature, fixed styling on width and height of the chart on resize
    
    * Added quickstart, added replaceme comments
    
    * Removed specific details on role filtering, cut duplciate cloning steps, adding explanation of delegated data fetching
    
    * more info on mongodb services, realm provider setup change
    
    * feat:CHARTS-3579 added validation to form
    
    * kristinas comments
    
    * New folder structure
    
    * removed old realm folder
    
    * Called out injected filters and dashboard filters in readme

---
+++ examples/authenticated-realm/src/index.js
@@ -0,0 +1,56 @@
+import ChartsEmbedSDK, { getRealmUserToken }from "@mongodb-js/charts-embed-dom";
+import { Stitch, UserPasswordCredential } from 'mongodb-stitch-browser-sdk'
+import "regenerator-runtime/runtime";
+
+const client = Stitch.initializeAppClient(
+  'realm-authentication-sample-eibkj', // Optional: ~REPLACE~ with your Realm App ID
+  { 
+    baseUrl: 'https://stitch-dev.mongodb.com' // Optional: ~REPLACE~ with your Realm URL
+  }
+); 
+
+function getUser() {
+  return document.getElementById("username").value;
+}
+
+function getPass() {
+  return document.getElementById("password").value;
+}
+
+function logOut() {
+  document.body.classList.toggle("logged-in", false);
+}
+
+document
+  .getElementById("login-page")
+  .addEventListener("input", () => document.body.classList.toggle("error", false));
+
+document
+  .getElementById("loginButton")
+  .addEventListener("click", () => tryLogin());
+
+document
+  .getElementById("logoutButton")
+  .addEventListener("click", () => logOut());
+
+async function tryLogin() {
+  const credential = new UserPasswordCredential(getUser(), getPass())
+  client.auth.loginWithCredential(credential).then(() =>
+  {
+    const sdk = new ChartsEmbedSDK({
+      baseUrl: "https://charts-dev.mongodb.com/charts-test2-pebbp", // Optional: ~REPLACE~ with your Charts URL
+      getUserToken: () => getRealmUserToken(client),
+    });
+
+    const chart = sdk.createChart({
+      chartId: "a2e775e6-f267-4c2c-a65d-fbf3fad4a4f2", // Optional: ~REPLACE~ with your Chart ID
+    });
+
+    chart.render(document.getElementById("chart"));
+    document.body.classList.toggle("logged-in", true);
+
+  }).catch(err => {
+    console.error("Authentication failed. If you are using the pre-built sample, please use one of the listed email addresses and use 'password' as the password.")
+    document.body.classList.toggle("error", true);
+  });
+}
