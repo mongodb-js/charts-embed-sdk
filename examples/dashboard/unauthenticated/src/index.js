f3154a4f2034420c0d8d695354dc4ba51c9367c8
khanguslee <khanguslee@gmail.com>
2022-02-15 15:57:20 +1100

    feat: Add theme toggle for unauthenticated dashboard example

--- examples/dashboard/unauthenticated/src/index.js
+++ examples/dashboard/unauthenticated/src/index.js
@@ -8,8 +8,21 @@ const dashboard = sdk.createDashboard({
   dashboardId: "6209eedf-cbeb-4b28-8efa-6577b324d6f1",
 });
 
+function addEventListeners() {
+  /* Theme toggle*/
+  document
+    .getElementById("theme")
+    .addEventListener("change", async function () {
+      await dashboard.setTheme(this.checked ? "dark" : "light");
+
+      const currentTheme = await dashboard.getTheme();
+      document.getElementById("currentTheme").innerText = currentTheme;
+    });
+}
+
 async function renderDashboard() {
   await dashboard.render(document.getElementById("dashboard"));
+  addEventListeners();
 }
 
 renderDashboard().catch((e) => window.alert(e.message));
